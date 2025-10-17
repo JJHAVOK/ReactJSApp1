f6ba96f2564b119d65be73d0bc53828120ff3d46
JJHAVOK <156384546+JJHAVOK@users.noreply.github.com>
2025-10-16 21:36:31 -0400

    done

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
-
-export default App;
-
-
 import ParallaxCard from './ParallaxCard';
 import './ParallaxCard.css'; // Make sure to import the CSS
 
